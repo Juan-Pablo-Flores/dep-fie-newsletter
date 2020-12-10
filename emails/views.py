2c5d2d7f7498c70f406342a99b8b1ca61dae94ed
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2020-12-10 05:46:06 -0600

    Added emails home template

--- emails/views.py
+++ emails/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("You're at the emails home page")
+    return render(request, 'emails/home.html')
