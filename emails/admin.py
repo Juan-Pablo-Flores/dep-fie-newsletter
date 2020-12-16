aa5bf205304634f2fb7b434c1b375b4abbdb5a01
Juan-Pablo-Flores <jpfloresd.97@gmail.com>
2020-12-16 12:56:08 -0800

    InterestGroup, Subscriber, SentEmail models

--- emails/admin.py
+++ emails/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import InterestGroup, Subscriber, Email
 
-# Register your models here.
+admin.site.register(InterestGroup)
+admin.site.register(Subscriber)
--- emails/models.py
+++ emails/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django import forms
+from django.contrib.auth.models import User
 
-# Create your models here.
+class InterestGroup(models.Model):
+    name = models.CharField('grupo', max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class Subscriber(models.Model):
+    name = models.CharField('nombre', max_length=250)
+    last_name = models.CharField('Apellido Paterno', max_length=250)
+    second_last_name = models.CharField('Apellido materno', max_length=250, blank=True, null=True)
+    email = models.EmailField('email')
+    degree = models.CharField('Ultimo grado de estudios', max_length=250, blank=True, null=True)
+    company = models.CharField('Compania', max_length=250, blank=True, null=True)
+    interest_groups = models.ManyToManyField(InterestGroup)
+
+    def __str__(self):
+        return self.name + " " + self.last_name + " " + self.second_last_name
+
+class SentEmail(forms.Form):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField('Asunto', max_length=250)
+    interest_groups = models.ManyToManyField(InterestGroup)
+    content = models.TextField('Contenido')
+    
+
